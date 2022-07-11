c452bba9ead8cb8f01dc0815b64e1272afa116d1
park4084 <akscjfals@naver.com>
2022-07-11 14:51:31 +0900

    AppendTerm

--- LinkedList_AddernTerm.c
+++ LinkedList_AddernTerm.c
@@ -21,3 +21,25 @@ ListHead *createLinkedList(void){
     L -> head = NULL;
     return L;
 }
+
+//다항식 리스트에 마지막 노드를 추가하는 연산
+void appendTerm(ListHead *L, float coef, int expo){
+    ListNode *newNode;
+    ListNode *p;
+    newNode = (ListNode *)malloc(sizeof(ListNode));
+    newNode -> coef = coef;
+    newNode -> expo = expo;
+    newNode -> link = NULL;
+
+    if(L ->head == NULL){   // 다항식 리스트가 공백인 경우  
+        L ->head = newNode; // L의 head가 newNode를 가르킴
+        return;
+    }
+    else{   // 다항식 리스트가 공백이 아닌 경우
+        p = L ->head;
+        while (p -> link != NULL){
+            p = p -> link;  //리스트의 마지막 노드를 찾음
+        }
+        p ->link = newNode; //새 노드 연결
+    }
+}
